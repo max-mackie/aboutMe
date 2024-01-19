018faf39cbf6962e0024ee3eea3a9aeac45baa3e
Max Mackie <maxmackie13@gmail.com>
2024-01-19 13:59:00 +0000

    feat:: added responsive element to work experience selector

--- src/app/Components/Work/Work.jsx
+++ src/app/Components/Work/Work.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import Job from "./Job/Job";
 import jobData from "./Job/jobData";
+import "./Work.css";
 
 const Work = () => {
   const [selectedButton, setSelectedButton] = useState(0);
@@ -26,15 +27,19 @@ const Work = () => {
         Where I've Worked
         <span className="flex-grow bg-dark-slate h-[1px] mx-2 "></span>
       </h2>
-      <div className="flex flex-col md:flex-row md:items-start  mt-4">
-        <div className=" mb-10 md:mb-0 relative">
-          <div className="absolute bg-dark-slate w-[2px] h-full" />
-          <div className=" absolute bg-green w-[2px]" style={highlightStyle} />
-          <div ref={buttonsRef} className="flex md:flex-col">
+      <div className="flex flex-col md:flex-row md:items-start mt-4">
+        {/* <div className=" mb-10 md:mb-0 relative"> */}
+        {/* <div className="absolute bg-dark-slate w-[2px] h-full" />
+          <div className=" absolute bg-green w-[2px]" style={highlightStyle} /> */}
+        <div className="my-responsive-width">
+          <div
+            ref={buttonsRef}
+            className="flex md:flex-col overflow-x-auto whitespace-nowrap"
+          >
             {jobData.map((data, index) => (
               <button
                 key={index}
-                className={`py-2 px-8 text-left whitespace-nowrap ${
+                className={`my-2 mx-8 text-left ${
                   index === selectedButton ? "text-green" : "text-slate"
                 }`}
                 onClick={() => selectButton(index)}
@@ -42,6 +47,10 @@ const Work = () => {
                 {data.company}
               </button>
             ))}
+            {/* </div> */}
+            {/* Horezontal for smaller screens */}
+            {/* <div className="absolute bg-dark-slate w-full h-[2px]" />
+            <div className=" absolute bg-green h-[2px]" style={highlightStyle} /> */}
           </div>
         </div>
         <div>
