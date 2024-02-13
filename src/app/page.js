2ad5be3aaa05d9f8f2c86b890a9e31d73bbed1d4
Max Mackie <maxmackie13@gmail.com>
2024-02-13 17:31:04 +0000

    adding facicon and page title

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Home from './Components/Home/Home';
-import './utils/fontawesome'
+import Head from 'next/head';
+import './utils/fontawesome';
 
 export default function App() {
   return (
     <div>
+      <Head>
+        <link rel="icon" href="/IMG_0562.jpg"/>
+        <title>Max Mackie</title>
+      </Head>
       <Home/>
     </div>
   )
